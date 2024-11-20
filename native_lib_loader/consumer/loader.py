d096cf78c081f248261a5a36aba88fe98dba1351
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-20 02:39:51 +0000

    Doc

--- native_lib_loader/consumer/loader.py
+++ native_lib_loader/consumer/loader.py
@@ -4,7 +4,12 @@ import importlib
 
 
 def load_library_module(module_name: str) -> None:
-    """Load the specified module, if it exists."""
+    """Load the specified module, if it exists.
+
+    The function allows the module to not exist so that it may be used by a consumer in
+    non-pip contexts (for example, if the library is instead installed by some other
+    package manager and so no wheel exists).
+    """
     try:
         module = importlib.import_module(module_name)
     except ModuleNotFoundError:
