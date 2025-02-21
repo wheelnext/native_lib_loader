7cee2dbcdc9529544f8bee01d654bbf68e5c066b
Vyas Ramasubramani <vyasr@nvidia.com>
2025-02-21 11:20:34 -0800

    Generalize structures for safety and flexibility

--- tests/generated_projects/templates/load.py
+++ tests/generated_projects/templates/load.py
@@ -9,10 +9,10 @@ loader = native_lib_loader.library.LibraryLoader(
     {
 # Write a jinja for loop over library_names
 {% for library_name in library_names %}
-        "{{ library_name }}": (
-            os.path.join(root, "lib", "lib{{ library_name }}.so"),
-            os.path.join(root, "lib", "lib{{ library_name }}.dylib"),
-            os.path.join(root, "lib", "{{ library_name }}.dll"),
+        "{{ library_name }}": native_lib_loader.library.PlatformLibrary(
+            Linux=os.path.join(root, "lib", "lib{{ library_name }}.so"),
+            Darwin=os.path.join(root, "lib", "lib{{ library_name }}.dylib"),
+            Windows=os.path.join(root, "lib", "{{ library_name }}.dll"),
         ),
 {% endfor %}
     },
